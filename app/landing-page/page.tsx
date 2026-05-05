42b436125dd85ad7ae5b6ec9d2516afff8ed9e84
Jung Code <buidinhtrung0704@gmail.com>
2026-05-05 23:12:11 +0700

    feat(workflow): rename home to landing feature

--- app/landing-page/page.tsx
+++ app/landing-page/page.tsx
@@ -1,15 +1,15 @@
-import { FeaturesGrid, HeroSection, HowItWorks } from '@/features/home';
+import { FeaturesGrid, HeroSection, HowItWorks } from '@/features/landing';
 
 const LandingPage = () => {
-	return (
-		<div className="flex flex-1 flex-col">
-			<main className="flex w-full flex-1 flex-col">
-				<HeroSection />
-				<HowItWorks />
-				<FeaturesGrid />
-			</main>
-		</div>
-	);
+  return (
+    <div className="flex flex-1 flex-col">
+      <main className="flex w-full flex-1 flex-col">
+        <HeroSection />
+        <HowItWorks />
+        <FeaturesGrid />
+      </main>
+    </div>
+  );
 };
 
 export default LandingPage;
